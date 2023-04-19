9d9bd4ae53fd03691d9f38d8e55251e919f2e6f7
H-V-7 <harsh.vardhan18@st.niituniversity.in>
2023-04-19 21:44:23 +0530

    Added Cookie Parser and erro handling

--- server/index.js
+++ server/index.js
@@ -1,8 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import {config} from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+//importing routes
+import userRoute from "./routes/UserRoute.js";
+import videoRoute from "./routes/VideoRoute.js";
+import commentRoute from "./routes/CommentRoute.js";
+import authRoute from "./routes/authRoute.js";
+
 const app = express(); //initializing express application.
 config(); //config will provide us the  variable which are specified in the .env file.
+
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth",authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/videos", videoRoute);
+app.use("/api/comments", commentRoute);
+
+
+//error handling middleware
+
+app.use((err,req,res,next) => {
+    const status = err.status || 500;
+    const message =err.message || "Something went wrong!";
+    return res.status(status).json({
+        success:false,
+        status:status,
+        message:message
+    })
+})
 
 //this function will be connecting our express application to the backend.
 const dbConnection = async () => {
